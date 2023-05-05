e68273560a5b4ad5f44d848af0f38190df3bd2ff
chubarev.s <chubarev_sergey@mail.ru>
2023-05-05 12:21:12 +0500

    Итоговый вариант

--- Program.cs
+++ Program.cs
@@ -14,8 +14,8 @@ int ReadData(string msg)
 //Метод заполнения массива
 string[] Gen1DArray(int len)
 {
-    string[] res=new string[len];
-    for(int i=0; i<len; i++)
+    string[] res = new string[len];
+    for (int i = 0; i < len; i++)
     {
         Console.Write("Введите элемент массива: ");
         string name = (string)(Console.ReadLine() ?? "0");
@@ -41,18 +41,38 @@ void Print1Darray(string[] arr)
 void ElementCount(string[] arr, int len)
 {
     int buf_len = 0;
-    string[] res = new string[len];
-    for(int i = 0; i<len; i++)
+    int count = 0;
+    string[] res = new string[len];  //создаём смежный массив
+    for (int i = 0; i < len; i++)
     {
-        buf_len = arr[i].Length;  
-        if(buf_len <4)
+        buf_len = arr[i].Length;
+        if (buf_len < 4)            //выборка эл-тов, длина которых не более 3х символов
         {
             res[i] = arr[i];
+            count++;                    //узнаём кол-во эл-тов нужной длины
+        }
+        else
+        {
+            res[i] = "9999";        //чтобы избежать заполнение нулями, заполняем любыми символоми, но более 3х
         }
     }
-    Print1Darray(res);
+    string[] arr_max3 = new string[count];      //смежный массив с нужным кол-вом эл-тов
+    for (int i = len - 1; i >= 0; i--)          //заполнение итогового массива
+    {
+        buf_len = res[i].Length;
+        if (buf_len < 4)                           
+        {
+            arr_max3[count - 1] = res[i];
+            count--;
+        }
+    }
+    Console.WriteLine("Выборка из эл-тов массива строк, длина которых меньше, либо равна 3 символам: ");
+    Print1Darray(arr_max3);
 }
 
-int len = ReadData("Введите длину массива");
+//Вводим длину массива
+int len = ReadData("Введите длину массива: ");
+//Формируем массив
 string[] Arr = Gen1DArray(len);
+//Производим выборку
 ElementCount(Arr, len);
